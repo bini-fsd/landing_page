908d4a6e2f246a1dd27e2b0fa36bde6198f98150
bini-fsd <77338405+bini-fsd@users.noreply.github.com>
2021-09-16 15:32:29 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,23 +1,54 @@
-# Landing Page Project
+# Project Title: ***Landing Page Project***
+
+## Demo-Preview
+[link](https://bini-fsd.github.io/landing_page/)
 
 ## Table of Contents
-** Project Description **
-** Usage **
-** Dependencies **
-** Demo Link **
+> Project Title
+> Demo-Preview
+> Table of Contents
+> Project Description
+> Installation
+> Usage
+> Development
+> Contribute
+	- Sponsor
+	- Adding new features or fixing bugs
+> License
+> Dependencies
+> Footer
 
 ## Project Description
 This ***Landing Page Project*** is allow me to clearly demonstrate how to design industry standard web app that
-help me understand how to transform a static web app to an interactive dynamic one. Thus, it help me to code:
-> build menu
-> scroll to page section using scrollTo method on a click event
-> set active sections that are being focused
+help me understand how to transform a static web app to an interactive dynamic one.
+
+## Installation
+1. Install any front-end web server
+2. Download the ZIP landing page app here [link](https://github.com/bini-fsd/landing_page)
+3. Extract the downloaded Zip landing page app
+4. Copy the extracted folder to the root directory of the web server
+5. Run the web server
+6. Open your web browser and type in localhost/index.html or 127.0.0.1/index.html
 
 ## Usage 
-This app is very important specially for those who needed a ***Single Page App***
+This web application is very important specially for those who needed a ***Single Page App***
+
+## Development
+This web application was developed by coding the following:
+> build a dynamic navigation menu
+> scroll to page section using scrollIntoView method on a click event
+> set active sections of the web app that is being focused
+
+## Contribute
+> If you can sponsor this web application, it can be improved and made available for educational purposes.
+> In case you face any issues while using the application or want to have additional feature, please report it back so that 
+  we can fix and improve the application by adding more features.
+
+## License
+This web application is available for educational purposes under the GNU public license [link](https://opensource.org/licenses/GPL-3.0), you can reuse the code or redistribute by notifying the developer.
 
 ## Dependencies
 This application doesn't have any dependency to run, just click on the demo link below
 
-## Demo Link
-[link](https://bini-fsd.github.io/landing_page/)
+## Footer
+*copyright* &copy; 2021
--- js/app.js
+++ js/app.js
@@ -18,30 +18,50 @@
  * 
 */
 
-/* During coding, www.w3schools.com, developer.mozilla.org, www.codegrepper.com, www.youtube.com
-   have been used as a reference
+/*  During coding, www.w3schools.com, developer.mozilla.org, www.codegrepper.com, www.youtube.com,
+    www.javascripttutorial.net have been used as a reference
 */
 
-let navList, links, anchorLink, sectionId;
-const navSections = ["Section1", "Section2", "Section3","Section4"];
+// Here five global variables and one array declared to store values and build dynamic navigation menu
+let navList, links, anchorLink, sectionId, mainSections;
+const navSections = [];
+//const navSections1 = ["Section1", "Section2", "Section3","Section4"];
+//const navSections = document.getElementsByTagName('section');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+
+/* Here helper functions build to construct the navigation and append them to the actual HTML document */
+
+// getParent method help to get the unordered list under the header tag
 function getParent(){
     return document.querySelector("#navbar__list");
 }
 
+// getMenuParent method help to return the nav element with a class of navbar__menu
 function getMenuParent(){
     return document.querySelector(".navbar__menu");
 }
 
+// insertMenu method take in the navigation menu build below and append it to the parent element
 function insertMenu(menu){
     const parent = getMenuParent();
     scrollToAnchor();
     return parent.appendChild(menu);
+}
+
+// extractSections method help to get the sections from the page and push it to the array declared globally
+function extractSections(){
+    mainSections = document.getElementsByTagName('section');
+
+    for(let index in mainSections){
+        if(index < mainSections.length){
+            navSections.push(mainSections[index].id);
+        }
+    }
 }
 
 /**
@@ -50,16 +70,18 @@ function insertMenu(menu){
  * 
 */
 
-// build the nav
+/*  build the nav: this method builds the dynamic navigation menu and actually insert the menu by calling the
+    insertMenu method
+*/
 function buildNavigation(navGrid){
     for(let navSection in navSections){
         navList = document.createElement("li");
         links = document.createElement("a");
-
+        
         //links.href = navSections[navSection].toLowerCase();
         links.innerHTML = navSections[navSection];
         links.classList.add("menu__link");
-        links.id = navSections[navSection];
+        //links.id = navSections[navSection];
 
         navList.appendChild(links);
         navGrid.appendChild(navList);
@@ -70,40 +92,47 @@ function buildNavigation(navGrid){
 }
 
 // Add class 'active' to section when near top of viewport
+
+/*  this method help to track the application by taking the active indicated index passed from the calling method
+    and put or append 'active' string in the class of the given links and sections
+*/
 function trackApp(index) {
     const current = document.getElementsByClassName("menu__link");
     
     for(let i in current){
         if(i != index){
             links[i].classList = "menu__link";
+            mainSections[i].classList = "";
         }
         else{
             links[index].className += " active";
+            mainSections[index].className = "active";
         }
     }
 }
 
 // Scroll to anchor ID using scrollTO event
+
+/*  This method build the detail parts of the links, indicate the anchorLink, get the target element and
+    tell the browser to scroll to a particular section in the page when the click event fired.
+*/
 function scrollToAnchor(){
+    let targetNo = 1;
+    let target;
+
     for(let navSection in navSections){
         sectionId = "#" + navSections[navSection].toLowerCase();
         anchorLink = document.getElementById(navSections[navSection]);
         links = document.getElementsByClassName("menu__link");
         links[navSection].href = sectionId;
+        target = document.getElementById("section"+targetNo);
 
-        anchorLink.addEventListener("click", (event) => {
-            const pageX = window.pageXOffset;
-            const pageY = window.pageYOffset;
-
-            window.scrollTo({
-                top: pageX,
-                left: pageY,
-                behavior: "smooth"
-            });
-
+        links[navSection].addEventListener("click", (event) => {
             trackApp(navSection);
-
+            target.scrollIntoView(true);
         }, false);
+
+        targetNo++;
     }
 }
 
@@ -112,7 +141,10 @@ function scrollToAnchor(){
  * Begin Events
  * 
 */
+
+// This is where the program starts to excute the moment the page completed loading its contents
 document.addEventListener("DOMContentLoaded", (event) => {
+    extractSections();
     buildNavigation(getParent());
 });
 
